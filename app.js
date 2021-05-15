d7d6dc6ba420cef0fb053a1c9b32c166f13fef18
Ximena <a329898@uach.mx>
2021-05-14 20:56:51 -0600

    Modifique estructura del proyecto, aniadi ubi.html representa la ubicacion de la empresa, su style stylesU y modifique rutas de archivos, agregue cosas en app.js

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((request, response) => {
-    const file = request.url == '/'? './index.html' : `./${request.url}`;
-    
-    fs.readFile(file, (err, data) => {
-        if(err){
-            response.writeHead(404, {"Content-Type":"text/plain"});
-            response.write("Not found");
-            response.end();
-        }else{
-            const extension = file.split('.').pop();
-            switch(extension){
-                case 'txt':
-                    response.writeHead(200, {"Content-Type":"text/plain"});
-                    break;
-                case 'html':
-                    response.writeHead(200, {"Content-Type":"text/html"});
-                    break;
-                case 'jpeg':
-                    response.writeHead(200, {"Content-Type":"image/jpeg"});
-                    break;
-                case 'css':
-                    response.writeHead(200, {"Content-Type":"text/css"});
-                    break;
-                case 'js':
-                    response.writeHead(200, {"Content-Type":"text/javascript"});
-                    break;
-                case 'ico':
-                    response.writeHead(200, {"Content-Type":"image/icon"});
-                    break;
-                default:
-                    response.writeHead(200, {"Content-Type":"text/plain"});
-            }
-            response.write(data);
-            response.end();
-        }
-        
-    });
-}).listen(8888);
+http.createServer((request, response) =>{
+ console.log(request.url);
+ const file = request.url == '/'? './WWW/index.html' : `./WWW${request.url}`;
 
+ fs.readFile(file, (err,data)=>{
+ if(err){
+  response.writeHead(404, {"Content-Type":"text/plain"});
+  response.write("Not Found");
+  response.end();
+ }else{
+  //"hola.como estas".split(".").pop() regresa el de la derecha
+  const extension = file.split('.').pop();
+  switch(extension){
+   case 'txt':
+    response.writeHead(200, {"Content-Type":"text/plain"});
+   break; 
+   case 'html':  
+    response.writeHead(200, {"Content-Type":"text/html"});
+   break;
+   case 'jpeg':  
+     response.writeHead(200, {"Content-Type":"image/jpeg"});
+    break;
+   case 'css':  
+     response.writeHead(200, {"Content-Type":"text/css"});
+    break;
+   case 'js':  
+     response.writeHead(200, {"Content-Type":"text/javascript"});
+    break;
+  }
+  response.write(data);
+  response.end();
+ }
+ });
+}).listen(8888);
 console.log("Servidor funcionando en http://localhost:8888/");
+
